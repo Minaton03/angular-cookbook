be3b09807a85b69c59d15e96a1567f5ad9b30ace
Minato <m-yamauchi@taisei-eng.co.jp>
2019-04-05 16:28:08 +0900

    主たるページへのリダイレクトとURLマッピングの設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeListComponent} from './recipe-list/recipe-list.component';
+import { from } from 'rxjs';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot([
+      {path: "", redirectTo: 'recipe-list', pathMatch: 'full'},
+      {path: "recipe-list", component: RecipeListComponent}
+    ])],
+  exports: [
+    RouterModule]
 })
 export class AppRoutingModule { }
