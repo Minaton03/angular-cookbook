a613a9bcd014d2d719c7e8c4b5833a14df825136
Minato <m-yamauchi@taisei-eng.co.jp>
2019-04-05 16:53:52 +0900

    recipeのデータモデルの作成、recipeの作成

---
+++ src/app/recipe/recipedata.ts
@@ -0,0 +1,38 @@
+import { Recipe } from './recipe';
+
+export const RECIPEDATA: Recipe[] = [{
+  id: 1,
+  name: "チキンライス",
+  minute: 15,
+  feature: "残りご飯で手早く。甘酸っぱいケチャップが鶏肉によく会う"
+},
+{
+  id: 2,
+  name: "カレーライス",
+  minute: 60,
+  feature: "安定感抜群。今日は何辛にしようかな"
+},
+{
+  id: 3,
+  name: "炊き込みご飯",
+  minute: 15,
+  feature: "食材を切ったらあとは炊飯器任せ"
+},
+{
+  id: 4,
+  name: "ツナピラフ",
+  minute: 15,
+  feature: "最も簡単な魚料理。野菜を切ったらあとは炊飯器任せ"
+},
+{
+  id: 5,
+  name: "チャーハン",
+  minute: 15,
+  feature: "簡単で味もあっさり。醤油と長ネギで和風のおかずにも"
+},
+{
+  id: 6,
+  name: "ドリア",
+  minute: 15,
+  feature: "チーズをたくさんかけて、オーブンへ"
+}];
